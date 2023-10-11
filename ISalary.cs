f6eddbebaf37be47b9650b2fac61319f39bd8efd
Ж - 15 < - 15@DESKTOP-D632EQF>
2023-10-11 21:34:30 +0300

    Добавьте файлы проекта.

---
+++ ISalary.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+        interface ISalary 
+        {
+            public string FName { get; set; }
+        public int WorkDays { get; set; }
+        public decimal CalculateSalary(int WorkDays);
+
+        }
+        interface ITax
+        {
+            public decimal CalculateTax(decimal Salary);
+        }
+
+       public class Manager : ISalary, Print
+        {
+            public string FName { get; set; }
+            public int WorkDays { get; set; }
+            public static decimal Salary;
+            
+            public decimal CalculateSalary(int WorkDays)
+            {
+                Salary = WorkDays * 1000;
+                return Salary;
+            }
+            public void Print()
+            {
+                Console.WriteLine("ФИО: " + FName);
+                Console.WriteLine("Кол-во рабочих дней: " + WorkDays);
+                Console.WriteLine("Зарплата: " + Salary);
+            }
+
+        }
+       public class DeptHead : ISalary, Print
+        {
+            public string FName { get; set; }
+            public int WorkDays { get; set;}
+            public static decimal Salary;
+
+            public decimal CalculateSalary(int WorkDays)
+            {
+                Salary = WorkDays * 2500;
+                return Salary;
+            }
+            public void Print()
+            {
+                Console.WriteLine("ФИО: " + FName);
+                Console.WriteLine("Кол-во рабочих дней: " + WorkDays);
+                Console.WriteLine("Зарплата: " + Salary);
+            }
+        }
+
+        class Accountant
+        {
+            public int WorkDays { get; set; }
+            public static decimal Tax;
+            public static decimal Salary { get; set; }
+            public decimal CalculateTax(ISalary Salary)
+            {
+                Tax = Salary.CalculateSalary(Salary.WorkDays) * (13 / 100);
+                return Tax;
+            }
+
+        }
+        
+        interface Print
+        {
+            public void Print();
+        }
+
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пила-струна. Стиль паука.");
+            Manager Manager1 = new();
+            Accountant Acc1 = new();
+            Manager1.FName = "Тестировщиков, Тестировщик Тестировщикович";
+            Manager1.WorkDays = 270;
+            Manager1.CalculateSalary(270);
+            Acc1.CalculateTax(Manager1);
+            Manager1.Print();
+            DeptHead DeptHead1 = new();
+            DeptHead1.FName = "Тестировщиков, Тестировщик Тестировщикович";
+            DeptHead1.WorkDays = 244;
+            DeptHead1.CalculateSalary(244);
+            Acc1.CalculateTax(DeptHead1);
+            DeptHead1.Print();
+        }
+
+        interface Interface
+        {
+            static Interface()
+            {
+
+            }
+            const int a = 10;
+            static int b;
+            public int c { get; set; }
+            public void Ball();
+        }
+        interface Interface2
+        {
+
+        }
+        class c1 { }
+        class c2 { }
+        class c3 : c1, Interface, Interface2
+        {
+            public int Abc { get; set; }
+            public int c { get; set; }
+            public void Ball()
+            {
+
+            }
+        } 
+        
+    }
+}
